be1b5499021f6ad21223255eaa2594657fb04b7b
Douglas Brunner <tempemail@example.com>
2025-06-11 17:54:42 -0400

    Refactor

--- crates/rust_server/src/lib.rs
+++
@@ -1,6 +0,0 @@
-use std::os::raw::c_char;
-
-#[no_mangle]
-pub extern "C" fn hello() -> *const c_char {
-    b"Hello from Rust!\0".as_ptr() as *const c_char
-}
--- crates/rust_shared/src/lib.rs
+++ crates/rust_shared/src/lib.rs
@@ -1,6 +1,6 @@
-use std::os::raw::c_char;
+use std::ffi::c_char;
 
 #[no_mangle]
 pub extern "C" fn hello() -> *const c_char {
-    b"Hello from Rust!\0".as_ptr() as *const c_char
+    c"Hello from Rust!".as_ptr()
 }
